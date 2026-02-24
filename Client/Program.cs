2300b42ffa7c94c315717dafa68100151e579fe1
Abdelrahman Hosnie Abdelradi <abdohosnie@gmail.com>
2026-02-24 23:04:04 +0200

    Data Communication HO2

---
+++ Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        // Setting
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Connect
+        IPAddress host = IPAddress.Parse("127.0.0.1");
+        IPEndPoint hostEndPoint = new IPEndPoint(host, 9090);
+        clientSocket.Connect(hostEndPoint);
+        Console.WriteLine("Connected to server!");
+
+        string clientMessage = "";
+        string serverMessage = "";
+
+        while (serverMessage.ToLower() != "bye" && clientMessage.ToLower() != "bye")
+        {
+            // Receive
+            byte[] recievedData = new byte[1024];
+            int receivedBytesLen = clientSocket.Receive(recievedData);
+
+            serverMessage = Encoding.UTF8.GetString(recievedData, 0, receivedBytesLen);
+            Console.WriteLine("Server Said: {0}", serverMessage);
+
+            Console.WriteLine("Write you message: ");
+            clientMessage = Console.ReadLine();
+
+            byte[] sendData = Encoding.UTF8.GetBytes(clientMessage);
+            clientSocket.Send(sendData);
+
+            Console.WriteLine("Message has been sent.");
+        }
+        Console.WriteLine("Convo Ended");
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        // Setting
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 9090);
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind
+        serverSocket.Bind(localEndPoint);
+
+        // Listen
+        serverSocket.Listen(5);
+        Console.WriteLine("Server is listening on port 9090...");
+
+        //Accept
+        Socket clientSocket = serverSocket.Accept();
+        Console.WriteLine("client connected!");
+
+        string serverMessage = "";
+        string clientMessage = "";
+
+        while (serverMessage.ToLower() != "bye" && clientMessage.ToLower() != "bye")
+        {
+            Console.WriteLine("Server: ");
+            serverMessage = Console.ReadLine();
+
+            byte[] sendData = Encoding.UTF8.GetBytes(serverMessage);
+            clientSocket.Send(sendData);
+            Console.WriteLine("Message has been sent.");
+
+            byte[] receiveData = new byte[1024];
+            int receivedBytesLen = clientSocket.Receive(receiveData);
+
+            clientMessage = Encoding.UTF8.GetString(receiveData, 0, receivedBytesLen);
+            Console.WriteLine("Client: " + clientMessage);
+        }
+        Console.WriteLine("Convo Ended");
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+        serverSocket.Close();
+    }
+}
